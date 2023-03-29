d06e1656aeb8ffe1de596247880e216708472140
CJ3016684 <CJ3016684@LAB01-PC14>
2023-03-29 12:42:10 -0300

    Adicionar arquivos de projeto.

---
+++ Lista4PR2/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercicio 1
+            //não é posivel atribuir uma variavel do tipo inteiro para o tipo float um numero com virgula nao sera aceito se for declarado numa variavel inteira um codigo possivel para reslver esse problema seria o seguinte:
+            //variavel = int.parse(console.readline()) ou variavel = float.parse(console.readline())
+
+
+            //exercicio2
+            //ReadLine() Retorna o valor da linha inteira. Console. Read() Retorna o valor em ASCII do primeiro caractere digitado na linha.
+
+
+            //exercicio 3 
+            //devemos tomar o cuidado de colocarmos o "parse" para mudar a variavel 
+
+
+            //exercicio 4 
+            // o proposito é diser se o numero é par ou impar. se entrarmos com o valor 8 a saida sera 1, se entrarmos com 27 a saida sera 2 e se esntrarmos com 28 a saida sera 1
+
+
+            //exercicio5
+
+            //    double d, t, df;
+
+            //    Console.WriteLine("qual a distancia:");
+            //    d = double.Parse(Console.ReadLine());
+            //    Console.WriteLine("qual o tempo:");
+            //    t = double.Parse(Console.ReadLine());
+
+            //    df = d / t;
+
+            //Console.WriteLine("a distancia final é:" + df);
+
+            //exercico6 
+
+
+            //int num;
+
+            //Console.WriteLine("qual o numero desejado:");
+            //num = int.Parse(Console.ReadLine());
+
+            //if (num % 3 == 0 && num % 5 == 0 && num % 10 == 0)
+            //    Console.WriteLine("o numero é divisivel por 3,5 e 10");
+            //else if (num % 3 == 0 && num % 5 == 0 && num % 10 != 0)
+            //{
+
+            //    Console.WriteLine("o numero é divisivel por 3 e 5 ");
+            //}
+            //else if (num % 3 == 0 && num % 5 != 0 && num % 10 != 0)
+            //{
+
+            //    Console.WriteLine("o numero é divisivel apenas por 3");
+            //}
+            //else if (num % 3 != 0 && num % 5 == 0 && num % 10 != 0)
+            //{
+
+            //    Console.WriteLine("o numero é divisivel apenas por 5");
+            //}
+
+
+            //exercicio 7
+
+
+            //int tem, km;
+
+            //Console.WriteLine("sua casa fica no km:");
+            //km = int.Parse(Console.ReadLine());
+
+            //if (km <= 12 && km >= 0 )
+            //    Console.WriteLine("voce ira demorar 71 minutos");
+            //else if (km <= 29 && km > 12)
+            //{
+
+            //    Console.WriteLine("voce ira demorar 57 minutos");
+            //}
+            //else if (km <= 55 && km >29)
+            //{
+
+            //    Console.WriteLine("voce ira demorar 30 minutos");
+            //}
+            //else if (km <= 82 && km > 55)
+            //{
+
+            //    Console.WriteLine("voce ira demorar 27 minutos");
+            //}
+            //else if (km <= 100 && km > 82)
+            //{
+
+            //    Console.WriteLine("voce ira demorar 12 minutos");
+            //}
+
+            //exercicio 8 
+
+
+            //double marc, quant, total, custo, imp, custof, luc;
+
+            //Console.WriteLine("=======MARCAS=======");
+            //Console.WriteLine("1 ROYAL");
+            //Console.WriteLine("2 WISKAS");
+            //Console.WriteLine("3 GOLDEN");
+            //Console.WriteLine("4 NUTRIEN");
+            //Console.WriteLine("5 SPECIALCAT");
+            //Console.WriteLine("6 MARBA");
+            //Console.WriteLine("7 GATUS");
+
+            //Console.WriteLine("escolha uma marca:");
+            //marc = double.Parse(Console.ReadLine());
+            //Console.WriteLine("escolha a quantidade");
+            //quant = double.Parse(Console.ReadLine());
+
+            //switch (marc)
+            //{
+
+            //    case 1:
+            //        total = 129.50 * quant;
+            //        custo = total * 0.775;
+            //        custof = total - custo;
+            //        imp = custof * 0.33;
+            //        luc = (total - custof) - imp;
+
+
+            //        break;
+            //    case 2:
+            //        total = 182.78 * quant;
+            //        custo = total * 0.897;
+            //        custof = total - custo;
+            //        imp = custof * 0.275;
+            //        luc = (total - custof) - imp;
+
+
+            //        break;
+            //    case 3:
+            //        total = 159.46 * quant;
+            //        custo = total * 0.717;
+            //        custof = total - custo;
+            //        imp = custof * 0.225;
+            //        luc = (total - custof) - imp;
+
+
+            //        break;
+            //    case 4:
+            //        total = 144.80 * quant;
+            //        custo = total * 0.889;
+            //        custof = total - custo;
+            //        imp = custof * 0.25;
+            //        luc = (total - custof) - imp;
+
+
+            //        break;
+            //    case 5:
+            //        total = 205.40 * quant;
+            //        custo = total * 0.8324;
+            //        custof = total - custo;
+            //        imp = custof * 0.22;
+            //        luc = (total - custof) - imp;
+
+
+            //        break;
+            //    case 7:
+            //        total = 133.99 * quant;
+            //        custo = total * 0.946;
+            //        custof = total - custo;
+            //        imp = custof * 0.20;
+            //        luc = (total - custof) - imp;
+
+
+            //        break;
+            //    case 6:
+            //        total = 125.10 * quant;
+            //        custo = total * 0.904;
+            //        custof = total - custo;
+            //        imp = custof * 0.179;
+            //        luc = (total - custof) - imp;
+
+
+            //        break;
+            }
+               
+
+
+
+
+
+
+
+            }
+    }
+}
